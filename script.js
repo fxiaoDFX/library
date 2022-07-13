7b29cd3b656761b28be9f7ec8373cff6aa33d915
Frankie Xiao <fxiaoX089@live.com>
2022-07-13 13:33:35 -0500

    add createCard() function

--- script.js
+++ script.js
@@ -16,13 +16,6 @@ Book.prototype.addBookToLibrary = function() {
 }
 
 /**
- * remove Book from array 
-*/
-Book.prototype.removeBook = function() {
-    myLibrary = myLibrary.filter(bookObj => bookObj != this);
-}
-
-/**
  * change read status of Book
  */
 Book.prototype.changeStatus = function (status) {
@@ -30,17 +23,56 @@ Book.prototype.changeStatus = function (status) {
 }
 
 /**
- * iterates through array and displays each element
+ * Brings popup menu where current book info is displayed and can be modified
  */
-function displayLibrary(){
+ Book.prototype.editBook = function() {
 
 }
 
 /**
- * Brings popup menu where current book info is displayed and can be modified
- */
-Book.prototype.editBook = function() {
+ * remove Book from array 
+*/
+Book.prototype.removeBook = function() {
+    myLibrary = myLibrary.filter(bookObj => bookObj != this);
+}
 
+/**
+ * iterates through array and displays each element
+ */
+function displayLibrary(){
+    
+}
+
+/**
+ * takes information for card and create card element
+ */
+function createCard(){
+    // create elements to be appended to document
+    const div_card = document.createElement('div');
+    const p_title = document.createElement('p');
+    const p_author = document.createElement('p');
+    const p_pages = document.createElement('p');
+    const button_container = document.createElement('div');
+    const button_edit = document.createElement('button');
+    const button_remove = document.createElement('button');
+
+    // add class names to elements
+    div_card.classList.add('card');
+    p_title.classList.add('title');
+    p_author.classList.add('author');
+    p_pages.classList.add('pages');
+    button_container.classList.add('button-container');
+    button_edit.classList.add('edit');
+    button_remove.classList.add('remove');
+    
+    // add text to buttons
+    button_edit.innerText = 'Edit';
+    button_remove.innerText = 'Remove';
+
+    button_container.append(button_edit, button_remove);
+    div_card.appendChild(button_container);
+    const book_shelf = document.querySelector('.book-shelf');
+    book_shelf.appendChild(div_card);
 }
 
 const book1 = new Book('harry potter');
