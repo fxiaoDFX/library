13bbd05536ced2a6d089c4edd32d3a5501ace6c4
Frankie Xiao <fxiaoX089@live.com>
2022-07-13 19:26:02 -0500

    version 1 of removeBook function()

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let myLibrary = [];
+const remove_buttons = null;
 
 // constructor
-function Book(title = '', author = '', pages = 0, status = false) {
+function Book(title = '', author = '', pages = '', status = false) {
+    const index = null;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -9,9 +11,10 @@ function Book(title = '', author = '', pages = 0, status = false) {
 }
 
 /**
- * add Book to array
+ * add Book to array and set its index property
  */
 Book.prototype.addBookToLibrary = function() {
+    this.index = myLibrary.length;
     myLibrary.push(this);
 }
 
@@ -32,60 +35,98 @@ Book.prototype.changeStatus = function (status) {
 /**
  * remove Book from array 
 */
-Book.prototype.removeBook = function() {
-    myLibrary = myLibrary.filter(bookObj => bookObj != this);
+function removeBook(index) {
+    myLibrary = myLibrary.filter(book => book.index != index);
+    fixIndex();
+    console.log(myLibrary);
+}
+
+/**
+ * fix index values of book objects in array and div#id
+ */
+function fixIndex() {
+    for(let i = 0; i < myLibrary.length; i++){
+        const target = document.getElementById(myLibrary[i].index);
+        target.id = i;
+        myLibrary[i].index = i;
+    }
 }
 
 /**
  * iterates through array and displays each element
  */
 function displayLibrary(){
-    
+    myLibrary.forEach(book => {
+        createCard(book);
+    });
+    remove_buttons = document.querySelectorAll('button.remove');
 }
 
 /**
  * takes information for card and create card element
  */
-function createCard(){
+function createCard(book){
     // create elements to be appended to document
     const div_card = document.createElement('div');
     const p_title = document.createElement('p');
     const p_author = document.createElement('p');
     const p_pages = document.createElement('p');
+    const div_status = document.createElement('div');
     const button_container = document.createElement('div');
     const button_edit = document.createElement('button');
     const button_remove = document.createElement('button');
 
     // add class names to elements
     div_card.classList.add('card');
+    div_card.setAttribute('id', book.index);
     p_title.classList.add('title');
     p_author.classList.add('author');
     p_pages.classList.add('pages');
+    div_status.classList.add('status');
     button_container.classList.add('button-container');
     button_edit.classList.add('edit');
     button_remove.classList.add('remove');
     
-    // add text to buttons
+    // add text to elements
+    p_title.innerText = book.title;
+    p_author.innerText = book.author;
+    p_pages.innerText = book.pages
+    div_status.innerText = book.status;
     button_edit.innerText = 'Edit';
     button_remove.innerText = 'Remove';
 
+    // append elements to document
     button_container.append(button_edit, button_remove);
-    div_card.appendChild(button_container);
+    div_card.append(p_title, p_author, p_pages, div_status, button_container);
     const book_shelf = document.querySelector('.book-shelf');
     book_shelf.appendChild(div_card);
 }
 
-const book1 = new Book('harry potter');
+/**
+ * get the index of an object in an array
+ */
+const index = (id) => myLibrary.map(object => object.index).indexOf(id);
+
+// TEST
+const book1 = new Book('harry potter', 'J.K. Rowling');
 const book2 = new Book();
 const book3 = new Book('apple');
-console.log(myLibrary);
 book1.addBookToLibrary();
 book2.addBookToLibrary();
 book3.addBookToLibrary();
-book2.removeBook();
-book1.removeBook();
-console.log(myLibrary);
-
 
 // TODO: event listeners for buttons
 // when clicking add book button, create a menu that enables user to enter in book info. Once done, display that book and allow that book to be delete for edited later.  
+displayLibrary();
+
+remove_buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const target = button.closest('.card');
+        const targetID = target.id;
+        target.remove();
+
+        // TODO: remove book from myLibrary
+        const index = parseInt(targetID);
+        removeBook(index);
+    })
+});
